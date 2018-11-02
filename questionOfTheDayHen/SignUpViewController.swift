73072e35b97becd3b886e5d26466f20e6a54b677
period3 <period3@p-162mba-09.local>
2018-11-02 10:37:11 -0500

    .

--- questionOfTheDayHen/SignUpViewController.swift
+++ questionOfTheDayHen/SignUpViewController.swift
@@ -7,29 +7,26 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func action(_ sender: UIButton)
+    {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!)
+        {
+            (authResult, error) in
+            guard let user = authResult?.user else {return}
+        }
     }
-    */
-
+    
 }
