0394ae02a1208f4b0ab02be24a3e54f97162b895
period3 <period3@p-162mba-05.d214.org>
2018-11-05 10:18:31 -0600

    class code view controller

---
+++ questionOfTheDayHen/classCodeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  classCodeViewController.swift
+//  questionOfTheDayHen
+//
+//  Created by period3 on 11/5/18.
+//  Copyright © 2018 period3. All rights reserved.
+//
+
+import UIKit
+
+class classCodeViewController: UIViewController {
+    @IBOutlet weak var classCodeTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
