bb5009709774fde27525d7e13d4fc4b4f1e0fc1b
period3 <period3@p-162mba-07.d214.org>
2018-11-06 10:13:52 -0600

    questionViewController

---
+++ questionOfTheDayHen/QuestionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QuestionViewController.swift
+//  questionOfTheDayHen
+//
+//  Created by period3 on 11/6/18.
+//  Copyright © 2018 period3. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    @IBOutlet weak var optionA: UIButton!    
+    @IBOutlet weak var optionD: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
